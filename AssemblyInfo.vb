0010deee90ca3b1e1d0c1eadbda46f774c288604
weeksre <47992916+weeksre@users.noreply.github.com>
2019-05-14 09:34:20 -0400

    Add files via upload

---
+++ AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("David Test Application")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Shore Lab KHRI")> 
+<Assembly: AssemblyProduct("David Test Application")> 
+<Assembly: AssemblyCopyright("Copyright © Shore Lab KHRI 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("876c1667-3a85-4304-a4fd-6aa162a54d2a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
